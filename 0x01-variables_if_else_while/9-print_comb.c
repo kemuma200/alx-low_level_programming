2aca9cc5869302f79fa62fd1dcaae578e944c535
kemuma <nailamonyonko@gmail.com>
2023-03-17 22:55:59 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int k;
+
+	for (k = '0'; d <= '9'; d++)
+	{
+		putchar(k);
+		if (k != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
