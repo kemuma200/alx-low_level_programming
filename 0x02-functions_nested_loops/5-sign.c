b2d3b27e125d9d143dac50c2faacff0ae5eac03c
kemuma <nailamonyonko@gmail.com>
2023-03-22 04:12:39 -0700

    fifth question

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_sign - Prints the sign of a digit
+ *
+ *@n: Integere under consideration
+ *
+ * Return: 1 if positive, 0 if zero and -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (-1)
+}
