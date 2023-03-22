cabbe0469249d9da1c601ab8e1c1031fc64996ca
kemuma <nailamonyonko@gmail.com>
2023-03-22 04:02:18 -0700

    removing the regex pattern

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 #include "main.h"
 /**
  * _isalpha - checks if character is found in the alphabet
- * @c - character to be considered
+ * @c: character to be considered
  *
  * Return : 1 if letter is part of alphabet, 0 if not
  */
