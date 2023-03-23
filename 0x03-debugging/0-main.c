c40cb86dadc62fe874355948d05063a8de69c053
kemuma <nailamonyonko@gmail.com>
2023-03-23 06:34:29 -0700

    debugging first task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - tess if function gives correct output
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
