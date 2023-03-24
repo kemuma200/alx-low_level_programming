d88327966e87aa137baa7b9d493f0786676da6a2
kemuma <nailamonyonko@gmail.com>
2023-03-23 19:33:33 -0700

    Remaining tasks

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+
+#include "holberton.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: Parameter to be printed.
+ */
+void print_number(int n)
+{
+	unsigned int numb = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		numb = -numb;
+	}
+
+	if ((numb / 10) > 0)
+		print_number(numb / 10);
+
+	_putchar((numb % 10) + '0');
+}
