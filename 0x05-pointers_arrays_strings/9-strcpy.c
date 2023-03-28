5d14d76a4a6b534aa3666f273a3557890a8a85d6
kemuma <nailamonyonko@gmail.com>
2023-03-28 08:48:34 -0700

    Betty 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strcpy - copies the string pointed to by src, 
+ * _strcpy - copies the string pointed to by src,
  *		including the terminating null byte
  *		 (\0), to the buffer pointed to by dest
  *@dest: new buffer
