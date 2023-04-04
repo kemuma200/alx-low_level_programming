285c6203587b59ee01d6f115ab4e8088a2faad51
kemuma <nailamonyonko@gmail.com>
2023-04-03 18:49:04 -0700

    3 correction

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int k;
 
 	k = 0;
-	for (i = 0;s[i] != ' ' && s[i] != '\0'; i++)
+	for (i = 0; s[i] != ' ' && s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
