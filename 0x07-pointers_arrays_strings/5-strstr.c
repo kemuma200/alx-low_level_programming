ffdc60d24aee45470039d3a2dd7e71ad6780a75e
kemuma <nailamonyonko@gmail.com>
2023-04-04 16:12:01 -0700

    5th task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,13 +1,35 @@
 #include "main.h"
 
 /**
+ *_strstr -function finds the first occurrence of
+ *	the substring needle in the string haystack.
+ *	The terminating null bytes (\0) are not compared
+ *@needle: substring
+ *@haystack: string being considered
  *
- *
- *
- *
- *
+ *Return: pointer to beginning substring
  */
+boolean *str_comp(
 char *_strstr(char *haystack, char *needle)
 {
-  
+	int i, j;
+
+	i = 0;
+	while (*(needle + i) != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (*(haystack + j) != '\0')
+	{
+		if (needle[0] == *(haystack + j))
+		{
+			if (*(needle + i) == *(haystack + i))
+			{
+				return (haystack + j);
+			}
+		}
+		j++;
+	}
+	return ('\0');
 }
