7887b79ce197429e3db9611b909ae8c4006f2958
kemuma <nailamonyonko@gmail.com>
2023-04-03 18:17:49 -0700

    2 and 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_strspn - number of bytes in the initial segment
+ *	of s which consist only of bytes from accept
+ *@s: initial string
+ *@accept: string determining prefix
+ *Return: bytes in prefix s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, k;
+
+	for (i = 0; i != s[i]; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				k++;
+			}
+		}
+	}
+	return (k);
+}
