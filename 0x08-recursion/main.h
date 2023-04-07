fa7f81287b59645c9ac7de3abb5a088aec44aa3a
kemuma <nailamonyonko@gmail.com>
2023-04-06 19:44:55 -0700

    Added putchar

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,8 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-
-void _puts_recursion(char *s)
+int _putchar(char c);
+void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
