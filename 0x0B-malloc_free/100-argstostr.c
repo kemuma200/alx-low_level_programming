ab7fb6138f642e9c119ec79b2d9aec16fe14ea07
kemuma <nailamonyonko@gmail.com>
2023-04-15 00:20:36 -0700

    It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ *argstostr -  concatenates all the arguments of your program
+ *@ac:argument count
+ *@av:argument pointer
+ *Return:pointer to a new string, or NULL if it fails
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, len;
+	char *p;
+
+	if (ac == NULL || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			len++;
+		}
+		len++;
+	}
+	len++;
+
+	p = malloc(len * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0, k = 0; av[i][j] != '\0'; j++, k++)
+			p[k] = av[i][j];
+		p[k] = '\n';
+	}
+	p[k++] = '\0';
+	return (p);
+
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+char **strtow(char *str)
+{
+  /* count the number of words */
+
+  /* count the number of words in 1D array */
+}
