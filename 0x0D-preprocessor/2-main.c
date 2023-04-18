fc106d34a6823b15eae789edcd20e47a86cc91ce
kemuma <nailamonyonko@gmail.com>
2023-04-17 19:14:00 -0700

    Object-like Macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+/**
+ *main - prints the name of teh file it was compiled from
+ *	followed by  anew line
+ *Return: 0
+ */
+int main()
+{
+  printf("%s\n", __BASE_FILE__);
+  return (0);
+}
