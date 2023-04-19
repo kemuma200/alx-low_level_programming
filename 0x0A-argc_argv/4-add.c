287e09cb7b497e246e7105c6283798d046e1407f
kemuma <nailamonyonko@gmail.com>
2023-04-19 10:09:00 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "ctype.h"
+#include "string.h"
 
 /**
  *main - adds positive numbers
@@ -10,27 +11,29 @@
  */
 int main(int argc, char **argv)
 {
-	int i, j, result;
+	int i, j, len, result;
+	char *p;
 
-	result = 0;
 	if (argc < 2)
 		printf("%d\n", 0);
-
 	else
 	{
+		result = 0;
 		for (i = 0; i < argc; i++)
 		{
-			for (j = 0; argv[i][j] != '\0'; j++)
+			p = argv[i];
+			len = strlen(p);
+			for (j = 0; j < len; j++)
 			{
-			  if (isdigit(argv[i][j] == 0))
+				if (isdigit(p[j]) == 0)
 				{
-					printf("%s\n","Error");
+					printf("%s\n", "Error");
 					return (1);
 				}
 			}
 			result += atoi(argv[i]);
 		}
+		printf("%d\n", result);
 	}
-	printf("%d\n", result);
 	return (0);
 }
