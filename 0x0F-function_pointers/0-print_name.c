910fdf193398e1d8ab75c0a135ab5551ffddc340
kemuma <nailamonyonko@gmail.com>
2023-04-20 15:43:13 -0700

    whatsmyname

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,10 +5,11 @@
  *print_name - prints a name
  *@name: name considered
  *@f: function pointer
- *
+ *Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL)
-		(f)(name);
+	if (f == NULL || name == NULL)
+		return;
+	f(name);
 }
