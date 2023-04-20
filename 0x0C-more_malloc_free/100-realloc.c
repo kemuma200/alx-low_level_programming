8d676126d5d40ca4432d61a711cc96cb36252e57
kemuma <nailamonyonko@gmail.com>
2023-04-19 22:49:37 -0700

    _calloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+}
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,7 +5,7 @@
  *_calloc - allocates memory for an array using malloc
  *@nmemb: number of elements
  *@size: size of elements
- *
+ *Return: pointer to allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
