ca4f65872134ae68be2ad51aaf34d972f1cd5877
kemuma <nailamonyonko@gmail.com>
2023-04-19 21:01:19 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *malloc_checked - allocates memory using malloc
+ *@b: parameter that needs to eb stored
+ *Return: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b);
+	if (p != NULL)
+		return (p);
+	exit(98);
+
+}
