bea2a89a5b886a1679c4fd904382d09483c2698e
kemuma <nailamonyonko@gmail.com>
2023-04-27 13:48:26 -0700

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *print_list - prints all elements of a list
+ *@h:pointer to the head
+ *Return: nodes printed
+ */
+size_t print_list(const list_t *h)
+{
+	size_t a = 0;
+	if (h)
+	{
+		printf("%d\n", h->str);
+		h = h->next;
+	}
+	else
+	{
+		printf("%s\n",  "[0](nil)");
+		a++;
+	}
+	return (a);
+}
