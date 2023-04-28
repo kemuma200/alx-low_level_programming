85e0f8512a06358b94a745115c716023b3b5608a
kemuma <nailamonyonko@gmail.com>
2023-04-27 19:15:40 -0700

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *list_len -gets number of elements in a linked list_t list
+ *@h:singly linked list
+ *Return: no. of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t a = 0;
+
+	while (h != NULL)
+	{
+		a++;
+		h = h->next;
+	}
+	return (a);
+}
