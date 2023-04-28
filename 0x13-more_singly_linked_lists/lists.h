87cf63e2911a6d5adcbde39d0deb270dfb82678f
kemuma <nailamonyonko@gmail.com>
2023-04-28 05:30:55 -0700

    Add node

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define __LISTS_H__
 
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -19,7 +20,7 @@ typedef struct listint_s
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
-
+listint_t *add_nodeint(listint_t **head, const int n);
 
 
 #endif
