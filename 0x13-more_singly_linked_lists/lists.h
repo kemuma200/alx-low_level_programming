d085e233c1664b9cbb7521e0694f4a0c0cab598f
kemuma <nailamonyonko@gmail.com>
2023-04-28 06:13:25 -0700

    Free list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,8 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -22,5 +24,9 @@ size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+
+
 
 #endif
