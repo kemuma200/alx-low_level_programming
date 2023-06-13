4f8ec47c36dc9d653584a2cf3d34df860e926aef
kemuma200 <naila.monyonko@gmail.com>
2023-06-13 12:18:42 -0700

    Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,7 +8,7 @@
  */
 int get_sqrt(int a, int n)
 {
-	if ( a * a > n)
+	if (a * a > n)
 		return (-1);
 	if (a * a == n)
 		return (a);
@@ -24,7 +24,7 @@ int get_sqrt(int a, int n)
 
 int _sqrt_recursion(int n)
 {
-	if ( n < 0)
+	if (n < 0)
 		return (-1);
 	return (get_sqrt(0, n));
 }
