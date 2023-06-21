88bb9427be3595034aa1ccc946106cd519d419ce
kemuma200 <naila.monyonko@gmail.com>
2023-06-21 01:38:36 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *malloc_checked - 
+ *@b: integer considered
+ *Return: pointer to allocated memory, else 98
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
