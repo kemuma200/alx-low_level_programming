8175857a9dc7a37e5ac7d92bbec53460f4538692
kemuma200 <naila.monyonko@gmail.com>
2023-06-21 23:36:47 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - prints a name
+ *@name:string to be printed
+ *@f:pointer to function
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+  if (f)
+    (f)(name);
+}
