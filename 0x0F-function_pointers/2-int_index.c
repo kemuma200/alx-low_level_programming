ee2db33be238f7eeb774f7fb00da338d3e1ab451
kemuma200 <naila.monyonko@gmail.com>
2023-06-23 00:48:02 -0700

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,24 +1,34 @@
 #include "function_pointers.h"
 
 /**
- *
- *
- *
+ *cmp - searches an integer array for a value
+ *@a: first value
+ *@b: second value
+ *Return: true if present, false of not
  */
 bool cmp(const void *a, const void *b)
 {
-	return ( *(int *)a == *(int *)b );
+	return (*(int *)a == *(int *)b);
 }
 
 /**
- *
- *
- *
- *
- *
+ *int_index - searches for an integer
+ *@array: array to eb searched
+ *@size: size of the array
+ *@cmp: comparison function
+ *Return: index of element, else  -1
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-  
+	unsigned int c;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (c = 0; c < size; c++)
+	{
+		if ((cmp)(array[c]))
+			return (c);
+	}
+	return (-1);
 }
