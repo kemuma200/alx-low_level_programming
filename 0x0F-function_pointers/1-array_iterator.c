3abf81c3cc8dcf8c6b8e1eed962675ebdc829007
kemuma200 <naila.monyonko@gmail.com>
2023-06-23 00:25:15 -0700

    If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,6 +10,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int a;
 
+	if (action == NULL || size == 0)
+		return;
+
 	for (a = 0; a < size; a++)
 		(action) (array[a]);
 
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,6 +5,18 @@
  *
  *
  */
+bool cmp(const void *a, const void *b)
+{
+	return ( *(int *)a == *(int *)b );
+}
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
