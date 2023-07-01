aad4fb814df0a34479f0a37ab09e8868ee5d1c3c
kemuma200 <naila.monyonko@gmail.com>
2023-07-01 12:57:03 -0700

    Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,9 +10,13 @@
  */
 void free_list(list_t *head)
 {
-	if (head == NULL)
-  		return;
-	free_list(head->next);
-	free(head->str);
-	free(head);
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->next);
+		free(head);
+		head = tmp;
+	}
 }
