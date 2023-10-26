b7e9b0ed65c1bb99c293432a289e4ea1de503ab3
kemuma200 <naila.monyonko@gmail.com>
2023-10-25 19:57:47 -0700

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,12 +12,12 @@ int pop_listint(listint_t **head)
 	listint_t *tmp;
 	int num;
 
-	if (!head)
+	if (!head || head == NULL)
 		return (0);
 	num = (*head)->n;
 	tmp = (*head)->next;
 	free(*head);
 	*head = tmp;
 
-	return(num);
+	return (num);
 }
