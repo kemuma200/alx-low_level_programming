5eb3e6bfdd4fca9269e48e4671008f429aa35f70
kemuma200 <naila.monyonko@gmail.com>
2023-12-15 18:04:58 -0800

    Task two

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+ *add - adds two numbers
+ *@a: firsrt digit
+ *@b: second digit
+ *Return: sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *sub - subtracts one digit from another
+ *@a: first digit
+ *@b: second digit
+ *Return: difference of the two numbers
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *mul - multiplies two numbers
+ *@a: first digit
+ *@b: second digit
+ *Return: product of two digits
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *div - divide one digit from another
+ *@a: first digit
+ *@b: second digit
+ *Return: division result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *mod - returns the remainder of the division of two digits
+ *@a: first digit
+ *@b: second digit
+ *Return: remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
